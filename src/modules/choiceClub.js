dae9547d75b51c7bf9b17c554424c5073f259edb
Zubarev Sergey <barclays13@mail.ru>
2019-10-04 19:59:17 +0300

    choiceClub

--- src/modules/choiceClub.js
+++ src/modules/choiceClub.js
@@ -3,16 +3,14 @@ const choiceClub = () => {
     nameClubs = clubsList.querySelector('ul'),
     club = nameClubs.querySelectorAll('li');
 
-    document.addEventListener('click', () => {
-
-        if (nameClubs.style.display = "none" ){
+    clubsList.addEventListener('click', () => {
+        if (nameClubs.hasAttribute("style") ){
+            nameClubs.removeAttribute("style");
+            return;
+        } 
+        if (nameClubs.style.display = "none") {
             nameClubs.style.display = "block";
-        } else {
-            console.log(2);
-            nameClubs.style.display = "none";
-            console.log(2,1);
         }
     });
 };
-
 export default choiceClub;
