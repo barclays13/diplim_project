f9853f55e4c8eb869f1c9199bf4eed750a358ae0
Zubarev Sergey <barclays13@mail.ru>
2019-10-05 19:54:54 +0300

    правки по head

--- src/modules/call.js
+++ src/modules/call.js
@@ -6,15 +6,20 @@ const call = () => {
 
     document.addEventListener("click", (event) => {
         const target = event.target;
+        console.log('target: ', target);
         if (target.classList.contains('close_icon') || target.classList.contains('overlay')){ 
             callbackForm.removeAttribute("style");
             freeVisitForm.removeAttribute("style");
         }
+
         if (target.classList.contains('open-popup')){
-            callbackForm.style.display = "block";
+            console.log(1);
+            freeVisitForm.style.display = "block";
         }
+
         if (target.classList.contains('callback-btn')){
-            freeVisitForm.style.display = 'block';
+            console.log(2);
+            callbackForm.style.display = 'block';
         }
     });
 };
